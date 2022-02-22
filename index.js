86b9e3d443f8c410baeecc92bb51d5c1a13f4f2b
United States CA <jamalhoseen@Jamal-Hoseens-Air.local>
2022-02-21 19:11:48 -0800

    project done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
-        // Building the deck
-        
         var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
         var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
         var deck = new Array();
         var players = new Array();
         var currentPlayer = 0;
 
-        function createDeck(){
+        function createDeck()
+        {
             deck = new Array();
             for (var i = 0 ; i < values.length; i++)
             {
@@ -21,21 +20,6 @@
                     var card = { Value: values[i], Suit: suits[x], Weight: weight };
                     deck.push(card);
                 }
-            }
-        }
-
-        function shuffle()
-        {
-            // for 1000 turns
-            // switch the values of two random cards
-            for (var i = 0; i < 1000; i++)
-            {
-                var location1 = Math.floor((Math.random() * deck.length));
-                var location2 = Math.floor((Math.random() * deck.length));
-                var tmp = deck[location1];
-
-                deck[location1] = deck[location2];
-                deck[location2] = tmp;
             }
         }
 
@@ -71,6 +55,21 @@
                 div_player.appendChild(div_hand);
                 div_player.appendChild(div_points);
                 document.getElementById('players').appendChild(div_player);
+            }
+        }
+
+        function shuffle()
+        {
+            // for 1000 turns
+            // switch the values of two random cards
+            for (var i = 0; i < 1000; i++)
+            {
+                var location1 = Math.floor((Math.random() * deck.length));
+                var location2 = Math.floor((Math.random() * deck.length));
+                var tmp = deck[location1];
+
+                deck[location1] = deck[location2];
+                deck[location2] = tmp;
             }
         }
 
@@ -130,6 +129,27 @@
             return el;
         }
 
+        // returns the number of points that a player has in hand
+        function getPoints(player)
+        {
+            var points = 0;
+            for(var i = 0; i < players[player].Hand.length; i++)
+            {
+                points += players[player].Hand[i].Weight;
+            }
+            players[player].Points = points;
+            return points;
+        }
+
+        function updatePoints()
+        {
+            for (var i = 0 ; i < players.length; i++)
+            {
+                getPoints(i);
+                document.getElementById('points_' + i).innerHTML = players[i].Points;
+            }
+        }
+
         function hitMe()
         {
             // pop a card from the deck to the current player
@@ -141,7 +161,6 @@
             updateDeck();
             check();
         }
-
 
         function stay()
         {
@@ -157,43 +176,42 @@
             }
         }
 
+        function end()
+        {
+            var winner = -1;
+            var score = 0;
 
-          // returns the number of points that a player has in hand
-          function getPoints(player)
-          {
-              var points = 0;
-              for(var i = 0; i < players[player].Hand.length; i++)
-              {
-                  points += players[player].Hand[i].Weight;
-              }
-              players[player].Points = points;
-              return points;
-          }
-  
-          function updatePoints()
-          {
-              for (var i = 0 ; i < players.length; i++)
-              {
-                  getPoints(i);
-                  document.getElementById('points_' + i).innerHTML = players[i].Points;
-              }
-          }
+            for(var i = 0; i < players.length; i++)
+            {
+                if (players[i].Points > score && players[i].Points < 22)
+                {
+                    winner = i;
+                }
 
-          function end()
-          {
-              var winner = -1;
-              var score = 0;
-  
-              for(var i = 0; i < players.length; i++)
-              {
-                  if (players[i].Points > score && players[i].Points < 22)
-                  {
-                      winner = i;
-                  }
-  
-                  score = players[i].Points;
-              }
-  
-              document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID;
-              document.getElementById("status").style.display = "inline-block";
-          }
+                score = players[i].Points;
+            }
+
+            document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID;
+            document.getElementById("status").style.display = "inline-block";
+        }
+
+        function check()
+        {
+            if (players[currentPlayer].Points > 21)
+            {
+                document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' LOST';
+                document.getElementById('status').style.display = "inline-block";
+                end();
+            }
+        }
+
+        function updateDeck()
+        {
+            document.getElementById('deckcount').innerHTML = deck.length;
+        }
+
+        window.addEventListener('load', function(){
+            createDeck();
+            shuffle();
+            createPlayers(1);
+        });
